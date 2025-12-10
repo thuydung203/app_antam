c6129bdeec8fae8d732be09fa200941ae076c526
hao <nguyenthihao280433@gmail.com>
2025-12-10 17:56:35 +0700

    Add setting screen UI

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'login.dart';
 import 'signup.dart';
 import 'roleselection.dart';
 import 'following.dart';
+import 'setting.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FollowPage(),   // Khi chạy app → mở LoadingPage đầu tiên
+      home: SettingPage(), // Khi chạy app → mở LoadingPage đầu tiên
     );
   }
 }
