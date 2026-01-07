294d6147b5d2ad51a0621d17906a76c28c29ff52
Thuyd <hathithuydung2003bn@gmail.com>
2026-01-07 21:14:48 +0700

    chore: update main.dart with AuthProvider and Firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:antam_app/roleselection.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:antam_app/providers/auth_provider.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
